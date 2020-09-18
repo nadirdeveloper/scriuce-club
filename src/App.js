e2d43e555c4d5b4f12e85e5640559c61a235e83d
nadirdeveloper <63139854+nadirdeveloper@users.noreply.github.com>
2020-09-18 18:50:03 +0500

    ini

--- src/App.js
+++ src/App.js
@@ -1,55 +1,127 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import './styles.css';
 import logo from "./images/logo.png";
 import profilePic from './images/profile.png'
-import {Navbar,Nav,Form,FormControl,Row} from 'react-bootstrap';
+import { Navbar, Nav, Form, Row } from 'react-bootstrap';
 import CardItem from "./components/cardItem";
- class App extends Component {
-   
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [
+        {
+          postId: 1,
+          postTag: "SUSPENSE",
+          postTagColor: "#FEDCD2",
+          postTitle: "Title of Book",
+          postWriter: "Writer Name",
+          postFund: 35,
+          postLike: 354,
+          postComment: 28,
+          postShare: 16,
+          postDescription: "Phasellus nec sem in justo pellentesque facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada egestas. "
+        },{
+          postId: 2,
+          postTag: "Mystery",
+          postTagColor: "#F37078",
+          postTitle: "Title of Book",
+          postWriter: "Writer Name",
+          postFund: 35,
+          postLike: 354,
+          postComment: 28,
+          postShare: 16,
+          postDescription: "Phasellus nec sem in justo pellentesque facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada egestas. "
+        },
+        {
+          postId: 3,
+          postTag: "Thriller",
+          postTagColor: "#FFE4BD",
+          postTitle: "Title of Book",
+          postWriter: "Writer Name",
+          postFund: 35,
+          postLike: 354,
+          postComment: 28,
+          postShare: 16,
+          postDescription: "Phasellus nec sem in justo pellentesque facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada egestas. "
+        },
+        {
+          postId: 4,
+          postTag: "LOREM",
+          postTagColor: "#6A8C67",
+          postTitle: "Title of Book",
+          postWriter: "Writer Name",
+          postFund: 35,
+          postLike: 354,
+          postComment: 28,
+          postShare: 16,
+          postDescription: "Phasellus nec sem in justo pellentesque facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada egestas. "
+        },{
+          postId: 5,
+          postTag: "Thrille",
+          postTagColor: "#FFE4BD",
+          postTitle: "Title of Book",
+          postWriter: "Writer Name",
+          postFund: 35,
+          postLike: 354,
+          postComment: 28,
+          postShare: 16,
+          postDescription: "Phasellus nec sem in justo pellentesque facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada egestas. "
+        }, {
+          postId: 6,
+          postTag: "SUSPENSE",
+          postTagColor: "#FEDCD2",
+          postTitle: "Title of Book",
+          postWriter: "Writer Name",
+          postFund: 35,
+          postLike: 354,
+          postComment: 28,
+          postShare: 16,
+          postDescription: "Phasellus nec sem in justo pellentesque facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada egestas. "
+        },
+      ]
+    }
+  }
   render() {
     return (
       <React.Fragment>
         <div className="container">
-        <div className="main-container">
-        </div>
-        <div className="side-orange">
-        </div>
-        <div className="main-content">
-          <Navbar bg="red" expand="lg">
-  <Navbar.Brand href="#home"><img src={logo} className="nav-logo" alt=""/></Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="justify-content-center mr-auto ml-auto">
-    <Nav.Link className="nav-active" href="#home"><FormControl type="text" placeholder="Search" className="mr-sm-2" /></Nav.Link>
-    </Nav>
-    <Form inline>
-    <Nav.Link className="nav-active" href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Blogs</Nav.Link>
-      <Nav.Link href="#link">Generes</Nav.Link>
-      <Nav.Link href="#link">top writes</Nav.Link>
-      <Nav.Link href="#link">contact us</Nav.Link>
-      <Nav.Link href="#link"><i className="fa fa-envelope-o" aria-hidden="true"></i></Nav.Link>
-      <Nav.Link href="#link"><i className="fa fa-bell-o" aria-hidden="true"></i></Nav.Link>
-      <Nav.Link href="#link"><img src={profilePic} className="profile-pic" alt=""/></Nav.Link>
-    </Form>
-  </Navbar.Collapse>
-</Navbar> 
+          <div className="main-container">
+          </div>
+          <div className="side-orange">
+          </div>
+          <div className="main-content">
+            <Navbar bg="red" expand="lg">
+              <Navbar.Brand href="#home"><img src={logo} className="nav-logo" alt="" /></Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="justify-content-center mr-auto ml-auto">
+                  <Nav.Link className="nav-active" href="#home">
+                    {/* <FormControl type="text" placeholder="Search" className="mr-sm-2" /> */}
+                  </Nav.Link>
+                </Nav>
+                <Form inline>
+                  <Nav.Link className="nav-active" href="#home">Home</Nav.Link>
+                  <Nav.Link href="#link">Blogs</Nav.Link>
+                  <Nav.Link href="#link">Generes</Nav.Link>
+                  <Nav.Link href="#link">top writes</Nav.Link>
+                  <Nav.Link href="#link">contact us</Nav.Link>
+                  <Nav.Link href="#link"><i className="fa fa-envelope-o" aria-hidden="true"></i></Nav.Link>
+                  <Nav.Link href="#link"><i className="fa fa-bell-o" aria-hidden="true"></i></Nav.Link>
+                  <Nav.Link href="#link"><img src={profilePic} className="profile-pic" alt="" /></Nav.Link>
+                </Form>
+              </Navbar.Collapse>
+            </Navbar>
 
-      <div className="all-posts">
-      
-        <h4>All Posts</h4>
-      <Row>
-      <CardItem color="#FEDCD2" />
-      <CardItem color="#F37078" />
-      <CardItem color="#FFE4BD" />
-      <CardItem color="#6A8C67" />
-      <CardItem color="#FFE4BD" />
-      <CardItem color="#FEDCD2" />
-      </Row>
-      </div>
+            <div className="all-posts">
+
+              <h4>All Posts</h4>
+              <Row>
+                <CardItem data={this.state.products} />
+              </Row>
+            </div>
+          </div>
         </div>
-        </div>
-        
+
       </React.Fragment>
     )
   }
--- src/components/cardItem.js
+++ src/components/cardItem.js
@@ -1,41 +1,55 @@
 import React from 'react'
 import imgUser from '../images/other_user.png';
-import {Image} from 'react-bootstrap'
+import { Image } from 'react-bootstrap'
 export default function CardItem(props) {
     return (
-        <div className="card">
+        <React.Fragment>
+            {
+                props.data.map((element) =>{
+                    return(
+                        <div className="card">
             <div className="card-cont">
-            <div className="card-tag" style={{backgroundColor:props.color}}>
-                <h6>Suspense</h6>
-            </div>
-            <div className="card-data">
-                <div className="title-img">
-                    <div className="card-title">
-                    <h6>
-                        Title Of the Book
+                <div className="card-tag" style={{ backgroundColor: element.postTagColor }}>
+                    <h6>{element.postTag}</h6>
+                </div>
+                <div className="card-data">
+                    <div className="title-img">
+                        <div className="card-title">
+                            <h6>{element.postTitle}
                     <br />
-                        <span className="card-writer">
-                    Writer Name
+                                <span className="card-writer">
+                                    {element.postWriter}
                     </span>
-                    </h6>
+                            </h6>
+                        </div>
+                        <div className="card-img">
+                            <Image src={imgUser} height="40" />
+                        </div>
                     </div>
-                    <div className="card-img">
-                    <Image src={imgUser} height="40" />
+                    <div className="card-detail">
+                        <p>{element.postDescription} <span>Read More</span> </p>
                     </div>
-                </div>
-                <div className="card-detail">
-                <p>Phasellus nec sem in justo pellentesque facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada egestas. <span>Read More</span> </p>
-                </div>
-                <div className="card-percentage">
-
-                </div>
-                <div className="card-react">
-                    <div className="card-icon"><i className="fa fa-heart-o"></i><span> 34</span></div>
-                    <div className="card-icon"><i className="fa fa-comment-o"></i><span> 34</span></div>
-                    <div className="card-icon"><i className="fa fa-paper-plane-o"></i><span> 34</span></div>
+                    <div className="card-percentage">
+                        <p>Fund Needed</p>
+                        <div className="progress-cont">
+                        <div className="progress">
+                            <div className="progress-bar" role="progressbar" style={{width:element.postFund+"%" ,height:"8px"}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>                        
+                        </div>
+                    <span>{element.postFund}%</span>
+                        </div>
+                        
+                    </div>
+                    <div className="card-react">
+                    <div className="card-icon"><i className="fa fa-heart-o"></i><span> {element.postLike}</span></div>
+                    <div className="card-icon"><i className="fa fa-comment-o"></i><span> {element.postComment}</span></div>
+                    <div className="card-icon"><i className="fa fa-paper-plane-o"></i><span> {element.postShare}</span></div>
+                    </div>
                 </div>
             </div>
-            </div>
-           </div>
+        </div>
+                    )
+                })
+            }
+        </React.Fragment>
     )
 }
